d3b4bb083f21233ac08b295fe97406e5b4d77bbc
mgbriones <53794443+mgbriones@users.noreply.github.com>
2021-09-03 11:54:49 -0400

    Delete base directory

--- base/src/semaforo/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(int argc, char const *argv[])
-{
-  printf("\nHOLI, I'm the SEMAFORO process and my PID is: %i\n ", getpid());
-  printf("SEMAFORO - Mi padre es: %d\n", getppid());
-  printf("SEMAFORO - Mi delay es: %s\n\n", argv[0]);
-}
