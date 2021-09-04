8a095fe9f8ef9812f5153d57ade88053c5ec99dd
mgbriones <mgbriones@uc.cl>
2021-09-04 13:18:54 -0400

    :Jack-O-Lantern:init espera a los hijos semaforos

--- base/src/dccuber/main.c
+++ base/src/dccuber/main.c
@@ -3,10 +3,24 @@
 
 #include "../file_manager/manager.h"
 
+//   :Jack-O-Lantern:
+
+
+void handle_prueba( int sig, siginfo_t *siginfo, void *context)
+  {
+    printf("Entro a la funcion\n");
+    int number = siginfo->si_value.sival_int;
+    printf("el numero recibido es: %i\n", number);
+    
+  }
+
 int main(int argc, char const *argv[])
-{
-  printf("PRUEBA >>>>>>>>>>>>>>>>>>>>>>>>>>");
+{ 
   printf("I'm the DCCUBER process and my PID is: %i\n", getpid());
+
+  // Variables
+  pid_t pid;
+  int status;
 
 
   
@@ -20,29 +34,35 @@ int main(int argc, char const *argv[])
   printf("\t- ");
   for (int i = 0; i < 3; i++)
   {
-    printf("\nDONDE ESTA? %s ", data_in->lines[0][i]);
+
     char* aux = data_in->lines[0][i];
-    int pid = fork();
+    pid = fork();
+
+    // Este es el HIJO
     if (pid == 0){
-      printf("\n%d esto es hijo\n", pid);
-      execl("./semaforo", aux, NULL);
-    } else {
-      printf("\n %d soy main\n", pid);
-    }
+      printf("\n[HIJO] %d \n", pid);      
+      execl("./semaforo", aux, NULL);   
+    } 
+
+    // Este es el PADRE
+    printf("\n[MAIN] %d \n", pid);
+    
     
   }
   printf("\n");
-
   printf("\t- ");
   for (int i = 0; i < 5; i++)
   {
     printf("%s, ", data_in->lines[1][i]);
   }
   printf("\n");
-
   printf("Liberando memoria...\n");
   input_file_destroy(data_in);
 
+  for( int i = 0; i < 3; i++){
+    pid = wait(&status);
+    printf("finalizo INNIT - padre: %d - hijo: %d\n", getpid(), pid);
+  } 
   
-  
+  return 0;
 }
--- base/src/semaforo/main.c
+++ base/src/semaforo/main.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
+#include "../file_manager/manager.h"
+
+
+/*
+void handle_prueba( int sig, siginfo_t *siginfo, void *context)
+  {
+    printf("Entro a la funcion\n");
+    int number = siginfo->si_value.sival_int;
+    printf("el numero recibido es: %i\n", number);
+    
+  }
+  */
 
 int main(int argc, char const *argv[])
 {
+
+  /*
+  printf("\nConectando . . .");
+  //connect_sigaction(SIGUSR1,handle_prueba);
   printf("\nHOLI, I'm the SEMAFORO process and my PID is: %i\n ", getpid());
   printf("SEMAFORO - Mi padre es: %d\n", getppid());
   printf("SEMAFORO - Mi delay es: %s\n\n", argv[0]);
+  */
+  printf("\nConectando . . .\n[SEMAFORO] mi PID: %i - mi PADRE: %d - mi DELAY: %s\n", getpid(), getppid(), argv[0]);
+
 }
