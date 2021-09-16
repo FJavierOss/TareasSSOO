663820b1d907c281e890441177145fc4978f5fbf
mgbriones <mgbriones@uc.cl>
2021-09-16 19:09:47 -0300

    subida 5

--- Tarea 2/base/src/data_structs/data_structs.c
+++ Tarea 2/base/src/data_structs/data_structs.c
@@ -5,7 +5,8 @@
 #include "data_structs.h"
 
 
-Process* process_init(int pid, int n_fabrica, char* nombre, int start_time, int arreglo[]){
+Process* process_init(int pid, int n_fabrica, char* nombre,
+                      int start_time, int* arreglo, int size_arreglo){
   Process* process = calloc(1, sizeof(Process));
 
   process -> pid = pid;
@@ -13,11 +14,10 @@ Process* process_init(int pid, int n_fabrica, char* nombre, int start_time, int 
   memcpy(process -> name, nombre, strlen(nombre));
   process -> status = 1;
   process -> start_time = start_time;
-
-  memcpy(process -> arreglo, arreglo, 10*4); // luego hay q hacerlo dinamnico colocando el largo del array
- 
   process -> pos_avance_arreglo = 0;
-  process -> quantum;
+  process -> quantum = 0;
+  process -> arreglo = arreglo;
+  process -> size_arreglo = size_arreglo;
 
   return process;
 }
@@ -29,6 +29,13 @@ Queue* queue_init()
   Queue* queue = malloc(sizeof(Queue));
   queue -> first = NULL;
   queue -> last = NULL;
+  queue -> elementos = 0;
+  queue -> size = 0;
+  
+  queue -> fabrica_1 = 0;
+  queue -> fabrica_2 = 0;
+  queue -> fabrica_3 = 0;
+  queue -> fabrica_4 = 0;
   return queue;
 }
 
@@ -36,10 +43,10 @@ Queue* queue_init()
 /** Inserta un elemento al principio de la cola */
 void queue_insert(Queue* queue, Process* process)
 {
-
+  queue -> size ++;
   // Creo el nodo de la cola a insertar
   Node* node = malloc(sizeof(Node));
-  node -> integer = NULL;
+  node -> integer = 0;
   node -> process = process;
   node -> next = NULL;
 
@@ -77,8 +84,78 @@ void queue_move_to_last(Queue* queue, int pid){
     }
     continua = 0;
   }
+}
+
+
+  // Si un valor dentro del arreglo hace match con el pid requerido
+// entonces se toma el nodo que lo contiene y se envia al final del arreglo
+void queue_move_to_first(Queue* queue, int pid){
+  Node* current = queue -> first;
+  Node* parent = current;
+  int continua = 1;
+
+  // Aqui se evalua el caso en que el primer elemento de la cola no es el que se pide
+  if(current->process->pid != pid){
+    // Se recorren todos los nodos buscando un valor que haga match
+    while ((current -> next)&&(continua)){
+
+      // Aqui si es que un valor hace match, entonces se extrae el nodo
+      // y se agrega al final de la cola, esto se hace con redefinicion de punteros
+      if(current->process->pid == pid){
+        Node* hijo = current -> next;
+        parent -> next = hijo;
+        current -> next = queue -> first;
+        queue -> first = current;
+        continua = 0;
+      }
+      if (continua){
+        parent = current;
+        Node* next = current -> next;
+        current = next;
+      }
+
+    }
+
+    if (current->process->pid == pid){
+      parent -> next = NULL;
+      current -> next = queue -> first;
+      queue -> first = current;
+    }
+  } 
+}
+
+/** Elimina el nodo con el match de la cola y retorna el proceso correspondiente*/
+Process* queue_pop(Queue* queue, int pid)
+{
+
+  Node* current = queue -> first;
+  Node* parent = current;
+  int continua = 1;
+
+  // Aqui se evalua el caso en que el primer elemento de la cola necesita
+  // sacarse
+  if(current->process->pid == pid){
+    if (current -> next){
+      Node* hijo = current -> next;
+      queue -> first = hijo;
+      current -> next = NULL;
+      Process* process = current -> process;
+      free(current);
+      queue -> size--;
+      return process;
+    } else {
+      queue -> first = NULL;
+      queue -> last = NULL;
+      Process* process = current -> process;
+      free(current);
+      queue -> size--;
+      return process;
+    }
+    continua = 0;
+  }
 
   // Se recorren todos los nodos buscando un valor que haga match
+  // Este while no considera el ultimo nodo
   while ((current -> next)&&(continua)){
 
     // Aqui si es que un valor hace match, entonces se extrae el nodo
@@ -86,42 +163,32 @@ void queue_move_to_last(Queue* queue, int pid){
     if(current->process->pid == pid){
       Node* hijo = current -> next;
       parent -> next = hijo;
-      queue -> last -> next = current;
-      queue -> last = current;
-      current -> next = NULL;
+      Process* process = current -> process;
+      free(current);
       continua = 0;
+      queue -> size--;
+      return process;
     }
     if (continua){
       parent = current;
       Node* next = current -> next;
       current = next;
     }
+  }
 
+  // Se chequea el último nodo
+  if (current -> process -> pid == pid){
+    queue -> last = parent;
+    Process* process = current -> process;
+    free(current);
+    queue -> size--;
+    return process;
+  } else {
+    // Si es que no se encontro el pid en ningun nodo
+    return NULL;
   }
 }
 
-/** Elimina el primer nodo de la cola y lo retorna */
-int queue_pop(Queue* queue)
-{
-
-  Node* poped = queue -> first;
-
-  // Si no quedan elementos retorno NULL
-  if (!poped) return 0;
-
-  // Sino, dejo el siguiente como first
-  queue -> first = poped -> next;
-
-  // Si no quedan nodos entonces last es NULL
-  if (!queue -> first) queue -> last = NULL;
-
-  // Obtengo el integer y libero el nodo
-  int integer = poped -> integer;
-  free(poped);
-
-  // Retorno el intger
-  return integer;
-}
 
 /** Libera la cola con todos sus nodos */
 void queue_destroy(Queue* queue)
@@ -131,6 +198,7 @@ void queue_destroy(Queue* queue)
   while (current)
   {
     Node* next = current -> next;
+    free(current->process->arreglo);
     free(current->process);
     free(current);
     current = next;
@@ -232,6 +300,70 @@ int get_quantum(Queue* queue, int num_fabrica, int q_entregado){
 
  
   return (q_entregado/(f * n_i)) ;
-  
+}
 
+///////////////////////////////////////////////////////////////////////
+////////////////////     Cola para Strings      ///////////////////////
+///////////////////////////////////////////////////////////////////////
+
+Queue_s* queue_init_s()
+{
+  Queue_s* queue = calloc(1, sizeof(Queue_s));
+  queue -> first = NULL;
+  queue -> last = NULL;
+  return queue;
+}
+
+
+
+void queue_insert_s(Queue_s* queue, char* info)
+{
+
+  // Creo el nodo de la cola a insertar
+  Node_s* node = malloc(sizeof(Node_s));
+  node -> info = info;
+  node -> next = NULL;
+
+  // Si la cola esta vacia, dejo el nodo como primero y ultimo
+  if (queue -> first == NULL)
+  {
+    queue -> first = node;
+    queue -> last = node;
+  }
+  // Sino, lo dejo como last y conecto el anterior last con el nodo
+  else
+  {
+    queue -> last -> next = node;
+    queue -> last = node;
+  }
+}
+
+void queue_destroy_s(Queue_s* queue)
+{
+  // Libera iteretivamente los nodos
+  Node_s* current = queue -> first;
+  while (current)
+  {
+    Node_s* next = current -> next;
+    free(current);
+    current = next;
+  }
+
+  // Libera la cola
+  free(queue);
+}
+
+void queue_print_s(Queue_s* queue){
+
+  Node_s* current = queue -> first;
+  printf("\n[");
+  while (current -> next){
+    printf("%s,", current -> info);
+
+    Node_s* next = current -> next;
+    current = next;
+  }
+  printf("%s,", current -> info);
+  printf("]\n");
+  printf("Se termino el recorrido\n");
 }
--- Tarea 2/base/src/scheduler/main.c
+++ Tarea 2/base/src/scheduler/main.c
@@ -6,251 +6,251 @@
 #include "../data_structs/data_structs.h"
 #include "../file_manager/manager.h"
 
+
 int main(int argc, char **argv)
 {
   printf("Hello T2!\n");
+  // Inicializamos la cola de lectura
+  Queue* cola_lectura = queue_init();
+  Queue* cola = queue_init();
 
   InputFile *file = read_file("input.txt");
+  
+  int numero_de_procesos = file->len;
+  cola -> elementos = numero_de_procesos;
 
   printf("Reading file of length %i:\n", file->len);
-  int dio = 1;
   for (int i = 0; i < file->len; i++)
   {
     char **line = file->lines[i];
 
-
+    int pid = i;
     char* nombre = line[0];
-    printf(
-        "\tProcess %s .\n", nombre);
-    int intit_time = atoi(line[1]);
-    printf("\t has init time of %d \n",intit_time);
+    int init_time = atoi(line[1]);
     int factory_n = atoi(line[2]);
-
-    printf("\t from factory %d \n",factory_n);
     int n_bursts = atoi(line[3]);
-    printf("\t and %d bursts.\n", n_bursts);
+    int* arreglo = calloc((n_bursts*2)-1, sizeof(int));
 
-
-
-    int bursts[(n_bursts*2)-1];
-    printf("burst size %ld\n", sizeof(bursts));
-
-    printf("[");
-    for (int cont = 0; cont < sizeof(bursts)/4; cont ++){
-      bursts[cont] = atoi(line[4+cont]);
-      printf("%d,", bursts[cont]);
+    for (int cont = 0; cont < (n_bursts*2)-1; cont ++){
+      arreglo[cont] = atoi(line[4+cont]);
     }
-    printf("]\n");
 
-    //printf("Size bursts %ld n_bursts %d\n", sizeof(bursts)/4, n_bursts);
-    printf(
-        "\tProcess %s from factory %d has init time of %d and %d bursts.\n",
-        nombre, factory_n, intit_time, n_bursts);
+    queue_insert(cola_lectura, process_init(pid, factory_n, nombre, init_time, arreglo, n_bursts));
+
+    
+    switch (factory_n) {
+
+    case 1:
+      cola -> fabrica_1 ++;
+      break;
+
+    case 2:
+      cola -> fabrica_2 ++;
+      break;
+
+    case 3:
+      cola -> fabrica_3 ++;
+      break;
+
+    default:  
+      cola -> fabrica_4 ++;
+    }
+
 
   }
   input_file_destroy(file);
-  
+
+
   //Valor por defecto de Q
-  int _q = 100;
+  int _q = 20;
 
-  Queue* cola = queue_init();
 
-  char* s1 = "proceso1";
   
-  int arreglo[9] = {1,2,3,4,1,1,1,1,2};
-  int arreglo_2[9] = {1,2,3,4,2,1,1,1,3};
- 
-  Process* p1 = process_init(1001,2, s1, 5, arreglo);
-  
-  char* s2 = "proceso2";
-  Process* p2 = process_init(1002,3, s2,7, arreglo_2);
 
-  char* s3 = "proceso3";
-  Process* p3 = process_init(1003,2, s3, 8, arreglo);
+
 
   
   
   // Aqui se deben insertar los nuevos procesos
 
   
-  /*
-  queue_insert(cola, p1);
-  queue_insert(cola, p2);
-  queue_insert(cola, p3);
-  
-  queue_print(cola);
-  */
   
   // Se iniciará con solo 1 PROCESO
 
   // Se ingresa el primer proceso a la lista ligada
-
-  queue_insert(cola, p1);
-  queue_insert(cola, p2);
-  printf(">> pid: %i\n", cola -> first->process->pid);
-  
-  switch (cola -> first -> process -> status) {
-    case RUNNING:
-      printf("En running\n");
-      break;
-
-    case READY:
-      printf("En ready\n");
-      break;
-
-    case WAITING:
-      printf("En waiting\n");
-      break;
-
-    case FINISHED:
-      printf("En finished\n");
-      break;
-
-    default:  
-      printf("Nothing\n");
-      break;
-  } 
-
-
+ 
+ 
+  int tiempo = 0;
+  Node* current;
+  Node* next;
+  Node* current_select;
+  Node* next_select;
 
   // Comienza el proceso
   
-  while (cola -> first){
+  while ((cola->fabrica_1)||(cola->fabrica_2)||(cola->fabrica_3)||(cola->fabrica_4)){
+    printf("=========== # ==========\n");
     
 
-    printf("\n");
-    printf(">PID: %i\n", cola->first->process->pid);
-    // Se elige a un proceso para running
-    cola -> first -> process -> status = 1;
-
-
-    int n_fabrica_aux = cola -> first -> process -> n_fabrica;
-    cola -> first -> process -> quantum = get_quantum(cola, n_fabrica_aux, _q);
+    // Aqui se revisa si existe un proceso dentro de la cola de lectura que su tiempo inicio
+    // sea igual al tiempo actual, y en ese caso lo agrega a la cola principal
+    
+    current = cola_lectura -> first;
+    for (int contador = 0; contador < cola_lectura -> size; contador++){
+      if (tiempo == current -> process -> start_time){
+        Process* p1 = queue_pop(cola_lectura, current -> process -> pid);
+        queue_insert(cola, p1);        
+        break;
+      }
+      next = current -> next;
+      current = next; 
+    }
+    
+    sleep(1);
+    
 
     //Se ejecuta el proceso
-    //int index = cola -> first -> process -> pos_avance_arreglo;
+
 
     
-    while (1){
-    
+    if (cola -> first){
+      printf("\n");
+      printf(">PID: %i\n", cola->first->process->pid);
       //printf(">>>\n");
 
-      cola -> first -> process -> arreglo[cola -> first -> process -> pos_avance_arreglo]--;
+      current_select = cola -> first;
+      for (int contador = 0; contador < cola -> size; contador++){
+        if(current_select -> process -> status == READY){
+          current_select -> process -> status = RUNNING;
+          int n_fabrica_aux = current_select -> process -> n_fabrica;
+          current_select -> process -> quantum = get_quantum(cola, n_fabrica_aux, _q);
+          queue_move_to_first(cola, current_select -> process -> pid);
+          break;
+        }
+
+        next_select = current_select -> next;
+        current_select = next_select;
+      }
+
+      printf("PID %d status %d \n",cola->first->process->pid, cola->first->process->status);
+      if (cola->first->next){
+        printf("PID %d status %d \n",cola->last->process->pid, cola->last->process->status);  
+      }
+      
+      printf("---\n");
+      
+
+      //resta el tiempo quantum que se le dedica al proceso
       cola -> first -> process -> quantum--;  
-  
-    
-    
-      // CAMBIO DE ESTADO
+      //resta el tiempo dedicado a los waiting y al running de la lista completa
+      current_select = cola -> first;
+      for (int contador = 0; contador < cola -> size; contador++){
+        if((current_select -> process -> status == RUNNING)||(current_select -> process -> status == WAITING)){
 
-      //largo del arreglo
-      int largo_arreglo = sizeof(cola -> first -> process -> arreglo)/sizeof(cola -> first -> process -> arreglo[0])-1;
-      /*
-      printf("==================\n");
-      printf("largo arreglo: %i\n", largo_arreglo);
-      printf("posicion en arreglo: %i\n",cola -> first -> process -> pos_avance_arreglo);
-      printf("n procesos: %i\n", cola -> first -> process -> arreglo[cola -> first -> process -> pos_avance_arreglo]);
-      printf("quantum: %i\n", cola -> first -> process -> quantum);
-      printf("==================\n\n");
-      */
-      //Finished
-      if (cola -> first -> process -> pos_avance_arreglo ==  largo_arreglo 
-      &&  cola -> first -> process -> arreglo[largo_arreglo] == 0){
+          
+          current_select ->process -> arreglo[current_select -> process -> pos_avance_arreglo]--;
 
-        //printf("Status cambia a FINISHED\n");
-        cola -> first -> process -> status = 3;
-        //elimina el primer elemento de la cola
-        printf("[FINALIZADO]>> pid: %i\n", cola -> first->process->pid);
-        queue_pop(cola);
-        
-        break;
-      }  
+          // CAMBIO DE ESTADO
+          //largo del arreglo
+          int largo_arreglo = (current_select -> process -> size_arreglo) - 1;
+          
+          printf("==================\n");
+          printf("largo arreglo: %i\n", largo_arreglo);
+          printf("posicion en arreglo: %i\n",current_select -> process -> pos_avance_arreglo);
+          printf("n procesos: %i\n", current_select -> process -> arreglo[current_select -> process -> pos_avance_arreglo]);
+          printf("quantum: %i\n", current_select -> process -> quantum);
+          printf("==================\n\n");
+          
+          //Finished
+          if (current_select -> process -> pos_avance_arreglo ==  largo_arreglo 
+          &&  current_select -> process -> arreglo[largo_arreglo] == 0){
 
-      //Waiting
-      else if (cola -> first -> process -> arreglo[cola -> first -> process -> pos_avance_arreglo] == 0){
-        printf("Status cambia a Waiting\n");
-        cola -> first -> process -> status = 2;
+            //printf("Status cambia a FINISHED\n");
+            current_select -> process -> status = 3;
+            //elimina el primer elemento de la cola
+            printf("[FINALIZADO]>> pid: %i\n", current_select->process->pid);
+            queue_pop(cola, current_select->process->pid);
+            
+            // se resta 1 a los elementos que se deben ejecutar
+            cola -> elementos--;
 
-        //se suma 1 al la posicion de avance del arreglo
-        cola -> first -> process -> pos_avance_arreglo++;
-        //Mueve este proceso al final de la cola
-        queue_move_to_last(cola,cola -> first -> process ->pid );
-        //printf(">> pid: %i\n", cola -> first->process->pid);
-        break;
+            switch (current_select -> process -> n_fabrica) {
+            case 1:
+              cola -> fabrica_1--;
+              break;
+
+            case 2:
+              cola -> fabrica_2--;
+              break;
+
+            case 3:
+              cola -> fabrica_3--;
+              break;
+
+            case 4:
+              cola -> fabrica_4--;
+              break;
+
+            default:  
+              printf("Error \n");
+              break;
+          }
+            
+            
+          }  
+
+          //Waiting
+          else if (current_select -> process -> arreglo[current_select -> process -> pos_avance_arreglo] == 0){
+            printf("Status cambia a Waiting\n");
+            current_select -> process -> status = 2;
+            
+
+            //se suma 1 al la posicion de avance del arreglo
+            current_select -> process -> pos_avance_arreglo += 2;
+            //Mueve este proceso al final de la cola
+            queue_move_to_last(cola,current_select -> process -> pid );
+            //printf(">> pid: %i\n", current_select->process->pid);
+            
+          }
+
+          //Ready
+          else if (current_select -> process -> quantum == 0){
+            printf("Status cambia a Ready\n");
+            current_select -> process -> status = 1;
+
+            //se suma 2 al la posicion de avance del arreglo, asi se salta el espacio de "descanzo"
+            current_select -> process -> pos_avance_arreglo += 2;
+            //Mueve este proceso al final de la cola
+            queue_move_to_last(cola,current_select -> process ->pid );
+            
+          }
+
+          //Running
+          else {
+            printf("Sigue en RUNNING\n");
+            
+          }
+
+
+        }
+
+        next_select = current_select -> next;
+        current_select = next_select;
       }
-
-      //Ready
-      else if (cola -> first -> process -> quantum == 0){
-        printf("Status cambia a Ready\n");
-        cola -> first -> process -> status = 1;
-
-        //se suma 1 al la posicion de avance del arreglo
-        cola -> first -> process -> pos_avance_arreglo++;
-        //Mueve este proceso al final de la cola
-        queue_move_to_last(cola,cola -> first -> process ->pid );
-        //printf(">> pid: %i\n", cola -> first->process->pid);
-        break;
-      }
-
-      //Running
-      else {
-        printf("Sigue en RUNNING\n");
-        
-      }
+     
 
   }
 
-
+  tiempo ++;
   }
 
 
 
 
 
-/*
-  printf("Proceso: pid %d; nFabrica %d\n", cola -> first -> process -> pid,
-  cola -> first -> process  -> n_fabrica);
-  printf("Proceso: nombre %s\n", cola -> first -> process -> name);
-  printf("Proceso: STATUS %i\n", cola -> first -> process -> status);
-
-  switch (cola -> first -> process -> status) {
-    case RUNNING:
-      printf("En running\n");
-      break;
-
-    case READY:
-      printf("En ready\n");
-      break;
-
-    case WAITING:
-      printf("En waiting\n");
-      break;
-
-    case FINISHED:
-      printf("En finished\n");
-      break;
-
-    default:  
-      printf("Nothing\n");
-      break;
-  }
-*/
+  printf("Salimos del wahile\n");
   queue_destroy(cola);
+  queue_destroy(cola_lectura);
 
 }
 
-
-/*
-if (cola -> first -> process -> status == 2){
-      printf("Paso a waiting\n");
-    }
-    else if (cola -> first -> process -> status == 3){
-      printf("Paso a finished\n");
-    }
-
-    else if (cola -> first -> process -> status == 1){
-      printf("Paso a ready\n");
-    }
-    else (cola -> first -> process -> status == 0){
-      printf("Continuo en running\n");
-    }
-*/
